85e4f80dc9f49651dc4f84447445791207d3ff39
shaddowlinkk <nathan.vanbrunt@gmail.com>
2020-05-25 15:24:10 -0400

    basic basic menus done

--- src/Rendering/Board.java
+++ src/Rendering/Board.java
@@ -6,48 +6,66 @@ public class Board extends JFrame{
     int width,height;
 
     public Board() {
-        int i=1;
-        GameScreen m = new GameScreen();
-/*        width=m.getWidth();
-        height=m.getHeight();*/
-        setSize(622,642);
-        EndScreen e = new EndScreen(622,642);
+        int i=0;
+        GameScreen gameScreen =null;
+        MainMenuScreen menuscreen = new MainMenuScreen(622,642);
+        EndScreen endScreen = new EndScreen(622,642);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-       add(m);
-       m.setFocusable(true);
-       m.setVisible(true);
-       setVisible(true);
-       while (true){
+        setSize(622,642);
+        setVisible(true);
+        add(menuscreen);
+        menuscreen.setVisible(true);
+
+       while (true) {
            try {
-               Thread.sleep(0,1);
+               Thread.sleep(0, 1);
            } catch (InterruptedException interruptedException) {
                interruptedException.printStackTrace();
            }
-           if(m!=null&&m.getDead()){
-               add(e);
-               m.setVisible(false);
-               e.setScore(m.getScore());
-               remove(m);
-               e.setVisible(true);
-               repaint();
-               m=null;
-           }
-           if(e!=null&&e.isOutState()){
-               if (e.isRespawnState()){
-                   m=new GameScreen();
-                   e.setVisible(false);
-                   remove(e);
-                   add(m);
-                   m.setFocusable(true);
-                   m.requestFocusInWindow();
-                   m.setVisible(true);
-                   repaint();
-                   e.reset();
-               }else {
-                   e.setVisible(false);
-                   remove(e);
-                   e.reset();
+           if (menuscreen.isPlaying()) {
+               if(i==0) {
+                   gameScreen = new GameScreen();
+                   menuscreen.setVisible(false);
+                   remove(menuscreen);
+                   add(gameScreen);
+                   gameScreen.setFocusable(true);
+                   gameScreen.requestFocusInWindow();
+                   gameScreen.setVisible(true);
+                   i++;
                }
+               if (gameScreen != null && gameScreen.getDead()) {
+                   add(endScreen);
+                   gameScreen.setVisible(false);
+                   endScreen.requestFocusInWindow();
+                   endScreen.setScore(gameScreen.getScore());
+                   remove(gameScreen);
+                   endScreen.setVisible(true);
+                   repaint();
+                   gameScreen = null;
+               }
+               if (endScreen != null && endScreen.isOutState()) {
+                   if (endScreen.isRespawnState()) {
+                       gameScreen = new GameScreen();
+                       endScreen.setVisible(false);
+                       remove(endScreen);
+                       add(gameScreen);
+                       gameScreen.setFocusable(true);
+                       gameScreen.requestFocusInWindow();
+                       gameScreen.setVisible(true);
+                       repaint();
+                       endScreen.reset();
+                   } else {
+                       endScreen.setVisible(false);
+                       menuscreen.setVisible(true);
+                       menuscreen.reset();
+                       add(menuscreen);
+                       remove(endScreen);
+                       endScreen.reset();
+                       i--;
+                   }
+               }
+           }else {
+               // non playing code
            }
        }
     }
--- src/Rendering/MainMenuScreen.java
+++ src/Rendering/MainMenuScreen.java
@@ -1,4 +1,77 @@
 package Rendering;
 
-public class MainMenuScreen {
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MainMenuScreen extends JPanel {
+    private JLabel title = new JLabel("Untitled game");
+    private JLabel play = new JLabel("play");
+    private Border b = BorderFactory.createLineBorder(Color.blue,5);
+    private boolean playing = false;
+    public MainMenuScreen(int width, int height){
+        setFocusable(true);
+        setLayout(null);
+        setVisible(true);
+        setBackground(Color.black);
+        setSize(width,height);
+            //title card
+            title.setFont(title.getFont().deriveFont(52f));
+            title.setSize(500,75);
+            title.setVerticalAlignment(SwingConstants.CENTER);
+            title.setHorizontalAlignment(SwingConstants.CENTER);
+            title.setForeground(Color.white);
+            title.setLocation(((width/2)-(title.getWidth()/2)),50);
+
+            play.setFont(play.getFont().deriveFont(25f));
+            play.setSize(100,50);
+            play.setBorder(b);
+            play.setVerticalAlignment(SwingConstants.CENTER);
+            play.setHorizontalAlignment(SwingConstants.CENTER);
+            play.setForeground(Color.white);
+            play.setLocation(((width/2)-(play.getWidth()/2)),250);
+            play.addMouseListener(new MouseListener() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    playing=true;
+                }
+
+                @Override
+                public void mousePressed(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseReleased(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseEntered(MouseEvent e) {
+                    play.setSize(125,50);
+                    play.setText(">play<");
+                    play.setLocation(((width/2)-(play.getWidth()/2)),250);
+
+                }
+
+                @Override
+                public void mouseExited(MouseEvent e) {
+                    play.setSize(100,50);
+                    play.setText("play");
+                    play.setLocation(((width/2)-(play.getWidth()/2)),250);
+
+                }
+            });
+            add(title);
+            add(play);
+        }
+
+    public boolean isPlaying() {
+        return playing;
+    }
+    public void reset(){
+        playing=false;
+    }
 }
