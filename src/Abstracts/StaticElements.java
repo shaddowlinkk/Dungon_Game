3483531c9214869bd363be7ccd38acdb136e7d19
shaddowlinkk <nathan.vanbrunt@gmail.com>
2020-05-25 22:41:53 -0400

    working on geting items to the inventory

--- src/Abstracts/StaticElements.java
+++ src/Abstracts/StaticElements.java
@@ -1,6 +1,7 @@
 package Abstracts;
 
 import Interfaces.BaseUncollidableObject;
+import Utils.Point;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -8,12 +9,15 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public abstract class StaticElements extends JLabel implements BaseUncollidableObject {
     int x,y;
     private BufferedImage im;
     private int nextSlot=1;
     private StandardCollidableObject[] inventory = new StandardCollidableObject[6];
+    Point[] locations = new Point[6];
 
     public StaticElements(){
         setVisible(false);
@@ -23,6 +27,8 @@ public abstract class StaticElements extends JLabel implements BaseUncollidableO
     public void setTexture(String fileName) {
         try {
             im = ImageIO.read(new File(".\\Assets\\Elements\\"+fileName));
+            locations=getPoints(fileName);
+            System.out.println(Arrays.toString(locations));
             setSize(im.getWidth(),im.getHeight());
         } catch (IOException e) {
             e.printStackTrace();
@@ -54,5 +60,25 @@ public abstract class StaticElements extends JLabel implements BaseUncollidableO
     }
     public void addToInventory(StandardCollidableObject item){
 
+    }
+    private Point[] getPoints(String fileName){
+        Point[] spawns = new Point[6];
+        BufferedImage img=null;
+        try {
+            img = ImageIO.read(new File(".\\Assets\\Elements\\"+fileName+"-points.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int g=0;
+        for (int i = 0; i < img.getWidth(); i++) {
+            for (int j = img.getHeight() - 1; j >= 0; j--) {
+                int clr = img.getRGB(i, j);
+                if (clr == -5955038){
+                    spawns[g]=new Point(i,j);
+                    g++;
+                }
+            }
+        }
+        return spawns;
     }
 }
--- src/Rendering/GameScreen.java
+++ src/Rendering/GameScreen.java
@@ -10,12 +10,16 @@ import Objects.BaseItem;
 import Objects.Player;
 import RoomGeneration.EnviromentGenerator;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class GameScreen extends JComponent {
